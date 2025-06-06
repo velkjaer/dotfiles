09828c464ce99183c4a4a5a17067fdc261e01a1b
Victor Elkjær Birk <vb@newsage.ai>
2025-06-06 16:00:38 +0200

    feat: updated fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    
-
     # GitHub Settings
     export GITHUB_USERNAME="velkjaer"
     export HISTTIMEFORMAT="%F %T "
@@ -9,29 +7,29 @@ if status is-interactive
     # Helm stuff
     export HELM_EXPERIMENTAL_OCI=1
 
+    # Keybindings
+    set -g fish_key_bindings fish_vi_key_bindings
+
     # Abbreviations
     abbr k kubectl
     abbr ktx kubectx
     abbr kns kubens
     abbr tf terraform
     abbr d docker
+    abbr clm colima
+    abbr clmu colima start
+    abbr clmd colima stop
+    abbr cons "code $HOME/Code/NewSage/newsage.code-workspace" 
+    abbr codf "code $HOME/dotfiles"
     abbr dco docker-compose
     abbr ghce "gh copilot explain"  
     abbr ghcs "gh copilot suggest"
 
+    alias claudec "npx @anthropic-ai/claude-code"
     alias rm="rm -i"
     alias cat="bat"
+    alias npm="pnpm"
 
-    if type -q starship
-        starship init fish | source    
-    end
-    
-    if type -q thefuck
-        thefuck --alias | source
-    end
-    
-    if type -q pyenv
-        pyenv init - | source
-        source (pyenv virtualenv-init -| psub)
-    end
+    starship init fish | source    
+
 end
